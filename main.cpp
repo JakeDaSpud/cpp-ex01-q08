0ab8230db4e9c8e2f4a6b8450388f044888f44f0
Jake O'Reilly <78006039+JakeDaSpud@users.noreply.github.com>
2024-01-30 08:58:42 +0000

    - Added drawShapes()
    - Finished Question

--- main.cpp
+++ main.cpp
@@ -10,29 +10,28 @@ using namespace std;
 void drawTriangle(int);
 void drawSquare(int);
 void drawRectangle(int, int);
+void drawShapes(int, int);
 
 int main() {
 
-    cout << "Printing 5*5 Pyramid (Triangle)...\n";
-    //drawTriangle(5);
-    drawTriangle(5);
+    drawShapes(5, 10);
 
-    cout << "\n\nPrinting 5*5 Square...";
-    drawSquare(5);
-
-    cout << endl;
-
-    cout << "\nPrinting 10*5 Rectangle...";
-    drawRectangle(10, 5);
+//    drawTriangle(5);
+//    drawSquare(5);
+//    drawRectangle(10, 5);
 
     return 0;
 }
 
 void drawRectangle(int length, int height) {
+
+    //rectangle row
     for (int i = 0; i < height; i++) {
 
+        //rectangle: go to next line
         cout << endl;
 
+        //rectangle column
         for (int j = 0; j < length; j++) {
             cout << "*  ";
         }
@@ -50,13 +49,15 @@ void drawTriangle(int height) {
     int numSpaces = 1;
 
     //print top to bottom
-    //iterate row
+    //triangle: row
+    //iterate row: triangle
     for(int i = 1, j = 0; i <= height; i++, j = 0) {
         //print spaces
         for(numSpaces = 1; numSpaces <= height - i; numSpaces++) {
             cout <<"  ";
         }
 
+        //triangle: column
         //print asterisks
         //i * row - 1 to get the num of asterisks, provides odd number to count up to
         while(j != ((2 * i) - 1)) {
@@ -66,5 +67,42 @@ void drawTriangle(int height) {
 
         //print next line
         cout << endl;
+    }
+}
+
+void drawShapes(int height = 5, int rectangleLength = 10) {
+    //do logic for all rows in one loop, one after another
+    for (int i = 1, j = 0; i <= height; i++, j = 0) {
+
+        //rectangle: go to next line
+        cout << endl;
+
+        //do logic for all columns in one loop, one after another in order
+        //rectangle column
+        for (int k = 0; k < rectangleLength; k++) {
+            cout << "*  ";
+        }
+
+        cout << "| ";
+
+        //square column
+        for (int k = 0; k < height; k++) {
+            //changed spacing of this string
+            cout << " * ";
+        }
+
+        cout << " |  ";
+
+        //triangle column
+        int numSpaces = 1;
+
+        for(numSpaces = 1; numSpaces <= height - i; numSpaces++) {
+            cout <<"  ";
+        }
+
+        while(j != ((2 * i) - 1)) {
+            cout << "* ";
+            j++;
+        }
     }
 }
